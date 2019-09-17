3a6cb20b3833f8e490be198572f6c41ff2597934
sanada08 <sarath.kumar@beldex.io>
2019-09-18 00:31:07 +0530

    updated the WASM file

--- monero_utils/MyMoneroBridgeClass_Base.js
+++ monero_utils/MyMoneroBridgeClass_Base.js
@@ -25,11 +25,39 @@
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// Original Author: Lucas Jones
+// Modified to remove jQuery dep and support modular inclusion of deps by Paul Shapiro (2016)
+// Modified to add RingCT support by luigi1111 (2017)
 //
-"use strict";
+// v--- These should maybe be injected into a context and supplied to currencyConfig for future platforms
+const MyMoneroBridge_utils = require('./MyMoneroBridge_utils')
 //
-const monero_config = require("./monero_config")
-const money_format_utils = require("../cryptonote_utils/money_format_utils")
-const instance = money_format_utils(monero_config)
+class MyMoneroBridgeClass_Base
+{
+	constructor(this_Module)
+	{
+		this.Module = this_Module;
+	}
+	//
+	//
+	__new_cb_args_with(task_id, err_msg, res)
+	{
+		const args = 
+		{
+			task_id: task_id
+		};
+		if (typeof err_msg !== 'undefined' && err_msg) {
+			args.err_msg = err_msg; // errors must be sent back so that C++ can free heap vals container
+		} else {
+			args.res = res;
+		}
+		return args;
+	}
+	__new_task_id()
+	{
+		return Math.random().toString(36).substr(2, 9); // doesn't have to be super random
+	}
+}
 //
-module.exports = instance;
+module.exports = MyMoneroBridgeClass_Base
